4319b0d416087db57064a4ccf62ecdbfce9d6cf5
barishaxxer <bariscankaxxer@gmail.com>
2025-01-14 22:20:33 +0300

    Module access

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.macrobyte.macrobyte {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.github.kwhat.jnativehook;
 
 
     opens com.macrobyte.macrobyte to javafx.fxml;
