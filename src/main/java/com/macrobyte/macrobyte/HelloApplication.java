bb46dac27f416051d810888dcf455a28c558e547
barishaxxer <bariscankaxxer@gmail.com>
2025-01-14 22:21:00 +0300

    Application Logic Updated

--- src/main/java/com/macrobyte/macrobyte/HelloApplication.java
+++ src/main/java/com/macrobyte/macrobyte/HelloApplication.java
@@ -1,23 +1,54 @@
 package com.macrobyte.macrobyte;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 public class HelloApplication extends Application {
+
+    public static KeyCode key;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        ModuleLayer.Controller controller = fxmlLoader.getController();
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+
+        stage.setTitle("MacroByte!");
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+               key = keyEvent.getCode();
+            }
+        });
         stage.setScene(scene);
         stage.show();
     }
 
+
     public static void main(String[] args) {
+
         launch();
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
     }
 }
+
+
+
