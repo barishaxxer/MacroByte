a15bca374b9157b8b8edd46da8fdde5decffbb06
barishaxxer <bariscankaxxer@gmail.com>
2025-01-15 14:56:28 +0300

    Feat: Apply Main Macro Logic

---
+++ src/main/java/com/macrobyte/macrobyte/MacroOperations.java
@@ -0,0 +1,19 @@
+package com.macrobyte.macrobyte;
+
+import javafx.fxml.FXMLLoader;
+
+import java.util.ArrayList;
+
+public class MacroOperations {
+
+    ArrayList<String> actionOrder;
+
+    public MacroOperations(){
+
+
+        actionOrder = new ArrayList<>();
+    }
+    public void runMacro(){
+
+    }
+}
