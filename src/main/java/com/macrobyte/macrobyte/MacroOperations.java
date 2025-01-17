e0d828058046e7085819d695fd86d01054c22562
barishaxxer <bariscankaxxer@gmail.com>
2025-01-17 16:43:45 +0300

    Reach getActions method

--- src/main/java/com/macrobyte/macrobyte/MacroOperations.java
+++ src/main/java/com/macrobyte/macrobyte/MacroOperations.java
@@ -2,18 +2,23 @@ package com.macrobyte.macrobyte;
 
 import javafx.fxml.FXMLLoader;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MacroOperations {
 
     ArrayList<String> actionOrder;
+    HelloApplication mainClass;
 
     public MacroOperations(){
 
-
         actionOrder = new ArrayList<>();
+        mainClass = new HelloApplication();
+
+
     }
     public void runMacro(){
+        System.out.println(HelloApplication.controller.getActions());
 
     }
 }
