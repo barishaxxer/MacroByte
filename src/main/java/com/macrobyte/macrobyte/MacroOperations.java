df40f8157240f776e5cd0096d23dc47d0647dcab
barishaxxer <bariscankaxxer@gmail.com>
2025-01-18 15:38:02 +0300

    Feat: Simulate left and right click

--- src/main/java/com/macrobyte/macrobyte/MacroOperations.java
+++ src/main/java/com/macrobyte/macrobyte/MacroOperations.java
@@ -2,23 +2,41 @@ package com.macrobyte.macrobyte;
 
 import javafx.fxml.FXMLLoader;
 
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.InputEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MacroOperations {
 
     ArrayList<String> actionOrder;
-    HelloApplication mainClass;
+    Robot robot;
 
     public MacroOperations(){
 
         actionOrder = new ArrayList<>();
-        mainClass = new HelloApplication();
+        try {
+            robot = new Robot();
+        }catch(AWTException a){
+            System.out.println("Something went wrong");
+        }
 
 
     }
     public void runMacro(){
-        System.out.println(HelloApplication.controller.getActions());
+        actionOrder.addAll(HelloApplication.controller.getActions());
+        for(String s : actionOrder){
+            if (s.strip().equals("Left Click")){
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+            }else if (s.strip().equals("Right Click")){
+                robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+            }
+
+        }
 
     }
 }
