0d44f0eeb9744c5daa282578506af0174667ceb8
barishaxxer <bariscankaxxer@gmail.com>
2025-01-21 13:45:55 +0300

    Symbol to string conversion

--- src/main/java/com/macrobyte/macrobyte/GlobalKeyListener.java
+++ src/main/java/com/macrobyte/macrobyte/GlobalKeyListener.java
@@ -5,18 +5,33 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GlobalKeyListener implements NativeKeyListener {
     public static String currentKey;
+    private static Map<String, String> keyMap = new HashMap<>();
+    static {
+        keyMap.put("⏎", "Enter");
+        keyMap.put("⌫", "Backspace");
+        keyMap.put("⇪", "Caps Lock");
+        keyMap.put("⇧", "Shift");
+        keyMap.put("⌃", "Ctrl");
+        keyMap.put("⌘", "Command");
+        keyMap.put("⌥", "Alt");
+        keyMap.put("⎋", "Esc");
+        keyMap.put("AltGr", "Alt");
+
+    }
     private String bufferKey;
     MacroOperations macro = new MacroOperations();
 
     public void nativeKeyPressed(NativeKeyEvent e) {
         bufferKey = NativeKeyEvent.getKeyText(e.getKeyCode());
+        bufferKey = keyMap.getOrDefault(bufferKey, bufferKey);
         System.out.println("Key Pressed: " + bufferKey);
         if (!bufferKey.equals("Undefined")){
-            if (bufferKey.equals("Meta")){
-                bufferKey = "Command";
-            }
+
             currentKey = bufferKey;
 
         }
