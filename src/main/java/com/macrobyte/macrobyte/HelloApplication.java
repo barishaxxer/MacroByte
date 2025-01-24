fb9305f678878fe770d2115528f020202bb984dc
barishaxxer <bariscankaxxer@gmail.com>
2025-01-24 17:39:22 +0300

    Fix

--- src/main/java/com/macrobyte/macrobyte/HelloApplication.java
+++ src/main/java/com/macrobyte/macrobyte/HelloApplication.java
@@ -25,7 +25,6 @@ public class HelloApplication extends Application {
 
         Scene scene = new Scene(fxmlLoader.load());
         controller = fxmlLoader.getController();
-
         stage.setTitle("MacroByte!");
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
